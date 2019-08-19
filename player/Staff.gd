f85b70d7451ef997dfb7c7191ca7e02f7c189d79
zachariah-spencer <spencerzachariah@gmail.com>
2019-08-19 10:28:57 -0500

    add staff and begin projectile work

--- player/Staff.gd
+++ player/Staff.gd
@@ -1,1 +1,11 @@
 extends Node2D
+
+func _physics_process(delta):
+	
+	rotation = (get_global_mouse_position() - global_position).angle() + PI / 2
+	
+	if Input.is_action_just_pressed('shoot'):
+		_shoot()
+
+func _shoot():
+	print('shot')

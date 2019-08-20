990878433b116751a37cd2b6a8a1b02eb67e74bc
zachariah-spencer <spencerzachariah@gmail.com>
2019-08-20 16:07:02 -0500

    add mana pellets on enemy drop

--- enemies/Enemy.gd
+++ enemies/Enemy.gd
@@ -6,6 +6,8 @@ var velocity = Vector2.ZERO
 export var hp := 1.0
 export var damage := 1.0
 export var gravity = 80
+export var mana_dropped := 1
+onready var mana := preload('res://enemies/ManaPellet.tscn')
 
 func _physics_process(delta):
 	if !is_on_floor():
@@ -21,4 +23,10 @@ func hit(by : Node2D, damage : float, type : int, knockback := Vector2.ZERO):
 
 
 func _die():
+	for i in range(0,mana_dropped):
+		var to_add = mana.instance()
+		to_add.global_position = global_position
+		to_add.velocity = Vector2.UP * Globals.CELL_SIZE * 2
+		to_add.rotation = rand_range(0,2 * PI)
+		get_tree().get_root().add_child(to_add)
 	queue_free()

b6733b52a58d784e224bf639b05d4b76e3b35536
cslaf <cade.schlaefli@gmail.com>
2019-08-28 18:17:51 -0500

    move spell folder to root

--- autoload/Spells.gd
+++ autoload/Spells.gd
@@ -1,8 +1,8 @@
 extends Node
 
 const SPELL_LIST = [
-preload("res://player/spells/BasicSpell.tscn"),
-preload('res://player/spells/FearSpell.tscn'),
-preload("res://player/spells/Spell.tscn"),
-preload('res://player/spells/IceBurstSpell.tscn'),
+preload("res://spells/BasicSpell.tscn"),
+preload('res://spells/FearSpell.tscn'),
+preload("res://spells/Spell.tscn"),
+preload('res://spells/IceBurstSpell.tscn'),
 ]

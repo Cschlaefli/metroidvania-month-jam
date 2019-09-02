621bde96d6faf4fa9d64aacfaa7ee7986442fd7e
cslaf <cade.schlaefli@gmail.com>
2019-09-02 17:12:45 -0500

    new teleport effect

--- spells/Teleport.gd
+++ spells/Teleport.gd
@@ -8,6 +8,22 @@ onready var right_check := $Right
 onready var left_bod := $LeftBod
 onready var right_bod := $RightBod
 
+var sprite_mat : ShaderMaterial
+
+var time = 0
+
+func _physics_process(delta):
+	if casting :
+		time += 1
+		sprite_mat.set_shader_param("amount", sin(time *5) *30)
+
+func start_casting():
+	casting = true
+
+func interupt():
+	if interuptable :
+		casting = false
+		sprite_mat.set_shader_param("amount", 0)
 
 func _ready():
 	left_check.cast_to = Vector2.LEFT* distance
@@ -19,7 +35,9 @@ func _ready():
 func cast(by : Node2D, point : Vector2 ,  d : Vector2):
 	if not by is Player :
 		return
-
+	
+	sprite_mat.set_shader_param("amount", 0)
+	
 	var dir := direction
 	var dist = distance
 	var check : RayCast2D

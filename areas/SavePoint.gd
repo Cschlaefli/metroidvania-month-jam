c565b714f44c45991c98d319ef182fbd2ca7aef6
cslaf <cade.schlaefli@gmail.com>
2019-09-03 00:36:51 -0500

    added save effects

--- areas/SavePoint.gd
+++ areas/SavePoint.gd
@@ -1,6 +1,6 @@
 extends Entrance
-tool
 var active := false
+
 
 func _process(delta):
 	var bods = get_overlapping_bodies()
@@ -8,9 +8,32 @@ func _process(delta):
 	for bod in bods :
 		if bod is Player :
 			active = true
+	if active :
+		$Particles2D.process_material.initial_velocity = 300.0
+	else :
+		$Particles2D.process_material.initial_velocity = 100.0
+
+onready var gradient : Gradient = $Particles2D.process_material.color_ramp.gradient
+onready var mid_color := gradient.get_color(1)
+
+onready var c_tween := Tween.new()
+export var activate_color := Color.blueviolet
+
+func _ready():
+	activate_color.a = .3
+	add_child(c_tween)
+	c_tween.interpolate_method(self, "set_mid_color", gradient.get_color(1), activate_color, .3, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+	c_tween.interpolate_method(self, "set_mid_color",  activate_color, gradient.get_color(1), .3, Tween.TRANS_LINEAR, Tween.EASE_IN, .3)
+
+func set_mid_color(color : Color):
+	gradient.set_color(1,color)
+
 
 func _input(event):
 	if active  and event.is_action_pressed("ui_accept") :
 		Globals.player.health = Globals.player.max_health
 		Globals.save()
-
+		if not c_tween.is_active() :
+			c_tween.interpolate_method(self, "set_mid_color", gradient.get_color(1), activate_color, .3, Tween.TRANS_LINEAR, Tween.EASE_OUT)
+			c_tween.interpolate_method(self, "set_mid_color",  activate_color, gradient.get_color(1), .3, Tween.TRANS_LINEAR, Tween.EASE_IN, .3)
+			c_tween.start()
